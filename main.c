2cc1b173cf539e775ab7d8258c2785f6699a0d2b
digenets <digenets@gmail.com>
2021-02-27 01:22:39 +0300

    support for variables included

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+#include <complex.h>
 #include "test.h"
 #include "pair_string_double.h"
 #include "linear_map.h"
@@ -49,6 +51,112 @@ int pop(char stack[MAX_SIZE][256], unsigned cur){
     return cur;
 }
 
+void input(int *expressions_count, int *vars_count, char expressions[SIZE1][SIZE2], char vars[SIZE1][SIZE2], char vars_names[SIZE1][SIZE2], char c_vars[SIZE1][SIZE2], char c_vars_names[SIZE1][SIZE2], int *c_vars_count) {
+    FILE *fin;
+    fin = fopen("input.txt", "r");
+    char file_read_str[SIZE2];
+    while (fgets(file_read_str, SIZE2, fin) != NULL) {
+        int len = (int)strlen(file_read_str) - 1;
+        int is_varuable = 0;
+        for (int i = 0; i < len; i++) {
+            if (file_read_str[i] == '=') {
+                is_varuable = 1;
+                break;
+            }
+        }
+        if (is_varuable == 1) {
+            char vars_without_spaces[SIZE2] = {'\0'};
+            int is_complex = 0;
+            for (int i = 0; i < len; i++) {
+                if (file_read_str[i] == 'j') {
+                    is_complex = 1;
+                    break;
+                }
+            }
+            if (is_complex == 1) {
+                int new_i = 0;
+                for (int i = 0; i < len; i++) {
+                    if (file_read_str[i] != ' ') {
+                        vars_without_spaces[new_i] = file_read_str[i];
+                        new_i++;
+                    }
+                }
+                new_i = 0;
+                int was_equal = 0;
+                for (int i = 0; i < strlen(vars_without_spaces); i++) {
+                    if (vars_without_spaces[i] == '=') {
+                        i++;
+                        new_i = 0;
+                        was_equal = 1;
+                    }
+                    if (was_equal == 0) {
+                        c_vars_names[*c_vars_count][i] = vars_without_spaces[i];
+                    } else {
+                        c_vars[*c_vars_count][new_i] = vars_without_spaces[i];
+                        new_i++;
+                    }
+                }
+                *c_vars_count = *c_vars_count + 1;
+            } else {
+                int new_i = 0;
+                for (int i = 0; i < len; i++) {
+                    if (file_read_str[i] != ' ') {
+                        vars_without_spaces[new_i] = file_read_str[i];
+                        new_i++;
+                    }
+                }
+                new_i = 0;
+                int was_equal = 0;
+                for (int i = 0; i < strlen(vars_without_spaces) - 1; i++) {
+                    if (vars_without_spaces[i] == '=') {
+                        i++;
+                        new_i = 0;
+                        was_equal = 1;
+                    }
+                    if (was_equal == 0) {
+                        vars_names[*vars_count][i] = vars_without_spaces[i];
+                    } else {
+                        vars[*vars_count][new_i] = vars_without_spaces[i];
+                        new_i++;
+                    }
+                }
+                *vars_count = *vars_count + 1;
+            }
+        } else if (file_read_str[0] != '\0' && file_read_str[0] != '\n'){
+            char expressions_without_spaces[SIZE2] = {'\0'};
+            int new_i = 0;
+            for (int i = 0; i < len; i++) {
+                if (file_read_str[i] != ' ' && file_read_str[i] != '\n') {
+                    expressions_without_spaces[new_i] = file_read_str[i];
+                    new_i++;
+                }
+            }
+            new_i = 0;
+            strcpy(expressions[*expressions_count], expressions_without_spaces);
+            *expressions_count = *expressions_count + 1;
+        }
+    }
+    fclose(fin);
+}
+
+void output(int *expressions_count, int *vars_count, char expressions[SIZE1][SIZE2], char vars[SIZE1][SIZE2], char vars_names[SIZE1][SIZE2], int expressions_results[SIZE1], char c_vars[SIZE1][SIZE2], char c_vars_names[SIZE1][SIZE2], int *c_vars_count) {
+    FILE *fout;
+    fout = fopen("output.txt", "w");
+    fprintf(fout, "vars:\n");
+    for (int i = 0; i < *vars_count; i++) {
+        fprintf(fout, "%s = %s\n", vars_names[i], vars[i]);
+    }
+    fprintf(fout, "\ncomplex vars:\n");
+    for (int i = 0; i < *c_vars_count; i++) {
+        fprintf(fout, "%s = %s\n", c_vars_names[i], c_vars[i]);
+    }
+    fprintf(fout, "\nexpressions:\n");
+    for (int i = 0; i < *expressions_count; i++) {
+        fprintf(fout, "%s = /*%d*/(result of calculation)\n", expressions[i], expressions_results[i]);
+    }
+    fclose(fout);
+}
+
 LINEAR_MAP ParseVariables(char** strings, int number_of_vars) {
     LINEAR_MAP map = CreateMap();
     for (int i = 0; i < number_of_vars; ++i) {
@@ -69,6 +177,9 @@ int main(int argc, char** argv) {
     char vars[SIZE1][SIZE2] = {'\0'};
     char vars_names[SIZE1][SIZE2] = {'\0'};
     int vars_count = 0;
+    char c_vars[SIZE1][SIZE2] = {'\0'};
+    char c_vars_names[SIZE1][SIZE2] = {'\0'};
+    int c_vars_count = 0;
     char funcs[13][SIZE2] = {'\0'};
     char funcs_names[13][SIZE2] = {
             "cos",
@@ -113,102 +224,131 @@ int main(int argc, char** argv) {
     }
     LINEAR_MAP vars_map = ParseVariables(input_strings, number_of_numbers);
 
-
-        for (int i = 0; i < strlen(str) - 1; i++) {
-            int is_func = 0;
-            int is_number = 0;
-            int k = 0;
-            char tmp_str[256] = { '\0' };
-            char tmp_func_str[256] = { '\0' };
-            if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
-                for (int j = i; j < strlen(str) - 1; j++) {
-                    if (is_oper(str, j)) {
-                        break;
-                    }
-                    if (str[j] == ')') {//WARNING
-                        break;
-                    }
-                    tmp_str[k] = str[j];
-                    k++;
+    for (int i = 0; i < strlen(str) - 1; i++) {
+        int is_func = 0;
+        int is_variable = 0;
+        int k = 0;
+        char tmp_str[256] = { '\0' };
+        char tmp_func_str[256] = { '\0' };
+        if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
+            for (int j = i; j < strlen(str) - 1; j++) {
+                if (is_oper(str, j)) {
+                    break;
                 }
-                for (int n = 0; n < strlen(tmp_str); n++) {
-                    if (tmp_str[n] == '(') {
-                        is_func = 1;
-                        is_number = 0;
-                        break;
-                    }
+                if (str[j] == ')') {//WARNING
+                    break;
                 }
-                if (!is_func) {
-                    is_func = 0;
-                    is_number = 1;
-                }
-                else {
-                    strcpy(tmp_func_str, tmp_str);
-                }        }
-            if (!is_func && is_number) {
-                cur_rpn = push(rpn, cur_rpn, tmp_str); // function "push" return cur_rpn++
-                i = i + k - 1;
-
+                tmp_str[k] = str[j];
+                k++;
             }
-            else if (is_func) {
-                char symb_zero[1];
-                symb_zero[0] = 40; //str where first element is "("
-                cur_oper_stack = push(oper_stack, cur_oper_stack, symb_zero); // 40 - ASCII CODE OF "("
-                // expon(23424+3423+2)
-                //count number of letters in name of function
-                int counter = 0;
-                for (int j = 0; j < strlen(tmp_func_str); j++) {
-                    if (tmp_func_str[j] == 40) {
-                        counter++;
-                        break;
-                    }
+            for (int n = 0; n < strlen(tmp_str); n++) {
+                if (tmp_str[n] == '(') {
+                    is_func = 1;
+                    is_variable = 0;
+                    break;
+                }
+            }
+            if (!is_func) {
+                is_func = 0;
+                is_variable = 1;
+            }
+            else {
+                strcpy(tmp_func_str, tmp_str);
+            }
+        }
+        if (!is_func && is_variable) {
+            cur_rpn = push(rpn, cur_rpn, tmp_str); // function "push" return cur_rpn++
+            i = i + k - 1;
+        }
+        else if (is_func) {
+            // expon(23424+3423+2)
+            //count number of letters in name of function
+            int counter = 0;
+            for (int j = 0; j < strlen(tmp_func_str); j++) {
+                if (tmp_func_str[j] == 40) {
                     counter++;
+                    break;
                 }
-                i = i + counter - 1;
-                for (int j = 0; j < strlen(tmp_func_str); j++) {
-                    if (j < counter - 1) {
-                        tmp_func_str[j] = tmp_func_str[j];
-                    } else {
-                        tmp_func_str[j] = '\0';
-                    }
-                }
-                cur_oper_stack = push(oper_stack, cur_oper_stack, tmp_func_str);
+                counter++;
             }
-            if (is_oper(str, i)) {
-                char tmp_top = top(oper_stack, cur_oper_stack);
-                char str_tmp_top[256] = {'\0'};
+            i = i + counter - 2;
+            for (int j = 0; j < strlen(tmp_func_str); j++) {
+                if (j < counter - 1) {
+                    tmp_func_str[j] = tmp_func_str[j];
+                } else {
+                    tmp_func_str[j] = '\0';
+                }
+            }
+            cur_oper_stack = push(oper_stack, cur_oper_stack, tmp_func_str);
+        }
+        else if (is_oper(str, i)) {
+            char tmp_top = top(oper_stack, cur_oper_stack);
+            char str_tmp_top[256] = {'\0'};
+            str_tmp_top[0] = tmp_top;
+            // Check that top of stack is operator
+            while  ( (top(oper_stack, cur_oper_stack) && is_oper(str_tmp_top, 0)) // пока стек не пустой и в топе оператор
+                     &&
+
+                     (
+                             /*либо*/((is_oper(str, i) == 1 || is_oper(str, i) == 2)/*токен левоасоц*/ && (is_oper(str, i) <= is_oper(str_tmp_top, 0))/*и приоритет токена меньше либо равен топу*/)
+                                     ||
+                                     /*либо*/((is_oper(str, i) == 3) /*токен правоасоц*/ && (is_oper(str, i) < is_oper(str_tmp_top, 0)) /*и приоритет токена меньше топа*/)
+                     )
+
+                     && is_oper(str_tmp_top, 0) != 40
+                    )
+            {//while cicle
+                tmp_top = top(oper_stack, cur_oper_stack);
                 str_tmp_top[0] = tmp_top;
-                // Check that top of stack is operator
-                while  ( (top(oper_stack, cur_oper_stack) && is_oper(str_tmp_top, 0)) // пока стек не пустой и в топе оператор
-                         &&
-
-                         (
-                                 /*либо*/((is_oper(str, i) == 1 || is_oper(str, i) == 2)/*токен левоасоц*/ && (is_oper(str, i) <= is_oper(str_tmp_top, 0))/*и приоритет токена меньше либо равен топу*/)
-                                         ||
-                                         /*либо*/((is_oper(str, i) == 3) /*токен правоасоц*/ && (is_oper(str, i) < is_oper(str_tmp_top, 0)) /*и приоритет токена меньше топа*/)
-                         )
-
-                         && is_oper(str_tmp_top, 0) != 40
-                        )
-                {//while cicle
-                    tmp_top = top(oper_stack, cur_oper_stack);
-                    str_tmp_top[0] = tmp_top;
-                    cur_rpn = push(rpn, cur_rpn, str_tmp_top);
-                    cur_oper_stack = pop(oper_stack, cur_oper_stack);
+                cur_rpn = push(rpn, cur_rpn, str_tmp_top);
+                cur_oper_stack = pop(oper_stack, cur_oper_stack);
+                //retake
+                tmp_top = top(oper_stack, cur_oper_stack);
+                for (int j = 0; j < 256; j++) {
+                    str_tmp_top[j] = '\0';
                 }
-                //end of while cicle
+                str_tmp_top[0] = tmp_top;
+            }
+            //end of while cicle
 
-                char str_tmp_oper[256] = { '\0' };
-                str_tmp_oper[0] = str[i];
-                cur_oper_stack = push(oper_stack, cur_oper_stack, str_tmp_oper);
+            char str_tmp_oper[256] = { '\0' };
+            str_tmp_oper[0] = str[i];
+            cur_oper_stack = push(oper_stack, cur_oper_stack, str_tmp_oper);
+        }
+        else if (str[i] == 40) {
+            char open_braket_str[256] = {'\0'};
+            open_braket_str[0] = 40; //str where first element is "("
+            cur_oper_stack = push(oper_stack, cur_oper_stack, open_braket_str);
+        }
+        else if (str[i] == 41) {
+            while (top(oper_stack, cur_oper_stack) != '(') {
+                char str_tmp_top[256] = {'\0'};
+                char tmp_top = top(oper_stack, cur_oper_stack);
+                str_tmp_top[0] = tmp_top;
+                cur_rpn = push(rpn, cur_rpn, str_tmp_top);
+                cur_oper_stack = pop(oper_stack, cur_oper_stack);
+            }
+            if (top(oper_stack, cur_oper_stack) == '(') {
+                cur_oper_stack = pop(oper_stack, cur_oper_stack);
+            }
+            if ((top(oper_stack, cur_oper_stack) >= 'A' && top(oper_stack, cur_oper_stack) <= 'Z') || (top(oper_stack, cur_oper_stack) >= 'a' && top(oper_stack, cur_oper_stack) <= 'z')) {
+                cur_rpn = push(rpn, cur_rpn, oper_stack[cur_oper_stack-1]);
+                cur_oper_stack = pop(oper_stack, cur_oper_stack);
             }
         }
-        while (!is_empty(oper_stack, cur_oper_stack)) {
-            char top_oper[256];
-            top_oper[0] = top(oper_stack, cur_oper_stack);
-            cur_rpn = push(rpn, cur_rpn, top_oper);
-            cur_oper_stack = pop(oper_stack, cur_oper_stack);
-        }
+    }
+    while (!is_empty(oper_stack, cur_oper_stack)) {
+        char top_oper[256];
+        top_oper[0] = top(oper_stack, cur_oper_stack);
+        cur_rpn = push(rpn, cur_rpn, top_oper);
+        cur_oper_stack = pop(oper_stack, cur_oper_stack);
+    }
+
+    printf("{");
+    for (int i = 0; i < cur_rpn; i++) {
+        printf("%s,",rpn[i]);
+    }
+    printf("}\n");
 
     return 0;
 }
